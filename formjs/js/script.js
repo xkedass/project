ccc45789e30da1e622dff5bc60c72caa9cea4409
Fatiha <sadeq.fatiha@hotmail.com>
2022-01-25 21:53:04 +0100

    maj

--- formjs/js/script.js
+++ formjs/js/script.js
@@ -1,24 +1,66 @@
 let inputName = document.querySelector('.nom');
 let inputPrenom = document.querySelector('.prenom');
+let inputDate = document.querySelector('.date');
+let inputMail = document.querySelector('.mail');
+let inputPassword = document.querySelector('.password');
+let inputconfirmPassword = document.querySelector('.confirmPassword');
 ///////////////////////////////////////////////////
-let button = document.querySelectorAll('button');
+let button = document.querySelector('.submit');
 /////////////////////////////////////////////////
-let sizeNom = nameTyped.length;
-let sizePrenom = prenomTyped.length;
+
 /////////////////////////////////////////////////////
-let nameTyped = document.querySelector('.nom').value;
-let prenomTyped = document.querySelector('.prenom').value;
+
+let count = 0;
 
 
 button.addEventListener('click', function (e) {
-    e.preventDefault(); 
-    console.log(nameTyped);
+    e.preventDefault();
+let sizeNom = document.querySelector('.nom').value.length;
+let sizePrenom = document.querySelector('.prenom').value.length;
+let nameTyped = document.querySelector('.nom').value;
+let prenomTyped = document.querySelector('.prenom').value;
          if (sizeNom > 2) {
-    console.log(prenomTyped);
-    inputName.insertAdjacentHTML('afterend','<p>Votre nom est </p>' + nameTyped);
+             console.log(sizeNom);
+    inputName.insertAdjacentHTML('afterend', `<p>Votre nom est </p> ${nameTyped}`);
+    count += count + 1 ;
         } else {
     inputName.insertAdjacentHTML('beforebegin','<p>Veuillez saisir au moins 2 lettres</p>');
-        };
+        }
+
+         if (sizePrenom > 2) {
+            inputPrenom.insertAdjacentHTML('afterend', `<p>Votre prenom est </p> ${prenomTyped}`);
+            count += count + 1 ;
+                } else {
+            inputPrenom.insertAdjacentHTML('beforebegin','<p>Veuillez saisir au moins 2 lettres</p>');
+                };
+
+        if(inputDate.value == '') {
+            inputDate.insertAdjacentHTML('beforebegin','<p>Veuillez saisir une date </p>'); 
+        }else{
+            count += count + 1;
+        }
+
+        if(inputMail.value.indexOf('@')) {
+              count += count + 1;
+        }else{
+            inputMail.insertAdjacentHTML('beforebegin','<p>Veuillez saisir un mail valide </p>');
+        }
+
+        if(inputPassword.value === inputconfirmPassword.value){
+            count += count + 1;
+        }else{
+            inputPassword.insertAdjacentHTML('beforebegin','<p> ils ne sont pas identiques </p>');
+        }
+        
+
+        if(count == 5) {
+            alert ('le formulaire est validé ! ');
+           document.querySelector('.valid').style.visibility = "visible";
+        } else {
+            alert ('le formulaire est incorrect ! ');
+            document.querySelector('.invalid').style.visibility = "visible";
+            
+        }
 })
 
  
