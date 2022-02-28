bb519c25d20f173910490f74a39de67590047ce0
xkedass <86850138+xkedass@users.noreply.github.com>
2022-02-28 16:43:14 +0100

    maj green garden

--- Green Garden FS/js/script.js
+++ Green Garden FS/js/script.js
@@ -1,7 +1,6 @@
   "use strict";
 let nav = document.querySelector('nav');
 let mousemove = document.querySelector('.mousemove');
-
 window.addEventListener("scroll", () => {
     console.log (window.scrollY);
     if(window.scrollY > 120) {
